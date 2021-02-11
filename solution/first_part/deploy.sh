f4f497a0d66dc205113d1885a9f7050cfc2bc743
Osman Elamin <osmasnays@gmail.com>
2021-02-11 08:50:27 +0300

    changes on code

--- solution/first_part/deploy.sh
+++ solution/first_part/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-docker build ../app/ -t osmansays/rundom
-docker push osmansays/random
+docker build ../app/ -t osmansays/random
+#docker push osmansays/random
 docker build ../reverseproxy/ -t osmansays/rproxy
-docker push osmansays/rproxy
+#docker push osmansays/rproxy
 docker-compose up -d
